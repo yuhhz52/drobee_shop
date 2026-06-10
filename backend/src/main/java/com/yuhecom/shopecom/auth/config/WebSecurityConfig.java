bc36e05e63f835f1dc164943743367d56e82273f
Huy Nguyen <thanhyuh0910@gmail.com>
2026-06-10 22:05:37 +0700

    ref: health check

--- backend/src/main/java/com/yuhecom/shopecom/auth/config/WebSecurityConfig.java
+++ backend/src/main/java/com/yuhecom/shopecom/auth/config/WebSecurityConfig.java
@@ -64,7 +64,7 @@ public class WebSecurityConfig {
 
     private static final String[] PUBLIC_APIS = {
             "/api/auth/**",
-            "/v1/api-docs/**", "/v1/docs/**", "/swagger-ui/**",
+            "/v1/api-docs/**", "/v1/docs", "/v1/docs/**", "/swagger-ui/**",
             "/oauth2/success", "/oauth2/tokens",
             "/uploads/**",
             "/api/orders/vnpay-return",
